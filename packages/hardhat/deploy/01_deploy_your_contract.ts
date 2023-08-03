34fcddf5e1447c19a5054425aee3fcff4c32fd80
Zak G <zakgriffith@gmail.com>
2023-08-03 13:57:07 -0600

    Deploy Script - Add sign.zakgriffith.eth as initial admin
    For testing purposes, adding an account I own as the initial admin

--- packages/hardhat/deploy/01_deploy_your_contract.ts
+++ packages/hardhat/deploy/01_deploy_your_contract.ts
@@ -30,7 +30,7 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   await deploy("YourContract", {
     from: deployer,
     // Contract constructor arguments
-    args: [deployer,ZERO_ADDRESS,[],[]],
+    args: ["0x7d0FdcDCb8876365E131091C9E10AA9AA8E863C3",ZERO_ADDRESS,[],[]],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
