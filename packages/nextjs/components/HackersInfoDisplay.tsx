4cca54a7517bf4f45f4a1e3732557be160c5b019
Pipoca <58862302+cacosta88@users.noreply.github.com>
2023-08-17 02:39:51 -0300

    Update HackersInfoDisplay.tsx
    update for time elapsed

--- packages/nextjs/components/HackersInfoDisplay.tsx
+++ packages/nextjs/components/HackersInfoDisplay.tsx
@@ -1,25 +1,51 @@
+import React, { useEffect, useState } from "react";
 import { Price } from "./Price";
-import { CreatorInfo } from "./homepage/StreamData";
 import { Address } from "./scaffold-eth";
+import { useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
 import { useCreatorUnlockedAmount } from "~~/hooks/useCreatorUnlockedAmount";
 import { getTimeAgo } from "~~/utils/getTimeAgo";
 
-// Component for displaying individual creator information
-export const HackersInfoDisplay: React.FC<{ creatorData: CreatorInfo; creatorAddress: string }> = ({
-  creatorData,
-  creatorAddress,
-}) => {
+interface CreatorInfo {
+  cap: string;
+  last: string;
+}
+
+interface HackersInfoDisplayProps {
+  creatorData: CreatorInfo;
+  creatorAddress: string;
+}
+
+export const HackersInfoDisplay: React.FC<HackersInfoDisplayProps> = ({ creatorData, creatorAddress }) => {
+  const [timeDifference, setTimeDifference] = useState<string>("");
+
   const { unlockedAmount } = useCreatorUnlockedAmount(creatorAddress);
   const cap = Number(creatorData.cap);
-  const last = Number(creatorData.last);
   const percentage = cap > 0 ? (Number(unlockedAmount) / cap) * 100 : 0;
+
+  const withdrawn = useScaffoldEventHistory({
+    contractName: "YourContract",
+    eventName: "Withdrawn",
+    fromBlock: Number(process.env.NEXT_PUBLIC_DEPLOY_BLOCK) || 0,
+    blockData: true,
+  });
+
+  useEffect(() => {
+    const lastEvent = withdrawn.data
+      ?.filter(event => event.args[0] === creatorAddress)
+      .sort((a, b) => b.block.timestamp - a.block.timestamp)[0]; // Sorting by timestamp
+
+    if (lastEvent) {
+      const timestamp = lastEvent.block.timestamp * 1000;
+      setTimeDifference(getTimeAgo(timestamp));
+    }
+  }, [creatorAddress, withdrawn.data]);
 
   return (
     <div className="flex flex-col md:flex-row place-items-center border-t px-6 py-2 w-full">
-      <div className="w-fit md:w-[50%]  md:ml-0 py-2">
+      <div className="w-fit md:w-[50%] md:ml-0 py-2">
         <Address address={creatorAddress} />
       </div>
-      <div className="flex flex-col gap-1 w-full md:w-[50%] text-sm ">
+      <div className="flex flex-col gap-1 w-full md:w-[50%] text-sm">
         <div className="flex flex-row justify-center">
           <div className="flex md:text-sm text-[0.7rem] font-semibold">
             Ξ
@@ -37,8 +63,8 @@ export const HackersInfoDisplay: React.FC<{ creatorData: CreatorInfo; creatorAdd
         </div>
         <div className="flex flex-row justify-center">
           <div className="md:text-sm text-[0.7rem] font-semibold flex flex-row items-center">
-            <div className=" tracking-tighter px-3">Last:</div>
-            <div className=" tracking-tighter ">{getTimeAgo(last * 1000)}</div>
+            <div className="tracking-tighter px-3">Last Withdrawn:</div>
+            <div className="tracking-tighter">{timeDifference}</div>
           </div>
         </div>
       </div>
