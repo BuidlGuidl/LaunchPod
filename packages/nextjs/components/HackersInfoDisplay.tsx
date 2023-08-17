ea8d5097c097d8c38aa820b5fecc7552669fd6ec
avelous <avelousujiri@gmail.com>
2023-08-17 16:07:14 +0100

    issue #35 - fixed last withdraw time for each creator

--- packages/nextjs/components/HackersInfoDisplay.tsx
+++ packages/nextjs/components/HackersInfoDisplay.tsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
 import { Price } from "./Price";
 import { CreatorInfo } from "./homepage/StreamData";
 import { Address } from "./scaffold-eth";
+import { useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
 import { useCreatorUnlockedAmount } from "~~/hooks/useCreatorUnlockedAmount";
 import { getTimeAgo } from "~~/utils/getTimeAgo";
 
@@ -11,9 +13,23 @@ export const HackersInfoDisplay: React.FC<{ creatorData: CreatorInfo; creatorAdd
 }) => {
   const { unlockedAmount } = useCreatorUnlockedAmount(creatorAddress);
   const cap = Number(creatorData.cap);
-  const last = Number(creatorData.last);
   const percentage = cap > 0 ? (Number(unlockedAmount) / cap) * 100 : 0;
 
+  const [withdrawnEvents, setWithdrawnEvents] = useState<any[] | undefined>([]);
+
+  const withdrawn = useScaffoldEventHistory({
+    contractName: "YourContract",
+    eventName: "Withdrawn",
+    fromBlock: Number(process.env.NEXT_PUBLIC_DEPLOY_BLOCK) || 0,
+    blockData: true,
+  });
+
+  useEffect(() => {
+    const events = withdrawn.data?.filter(obj => obj.args[0] === creatorAddress);
+    setWithdrawnEvents(events);
+  }, [withdrawn.isLoading, creatorAddress, withdrawn.data]);
+
+  console.log(withdrawnEvents);
   return (
     <div className="flex flex-col md:flex-row place-items-center border-t px-6 py-2 w-full">
       <div className="w-fit md:w-[50%]  md:ml-0 py-2">
@@ -22,7 +38,6 @@ export const HackersInfoDisplay: React.FC<{ creatorData: CreatorInfo; creatorAdd
       <div className="flex flex-col gap-1 w-full md:w-[50%] text-sm ">
         <div className="flex flex-row justify-center">
           <div className="flex md:text-sm text-[0.7rem] font-semibold">
-            Ξ
             <span className="px-1">
               <Price value={Number(unlockedAmount)} />
             </span>
@@ -38,7 +53,11 @@ export const HackersInfoDisplay: React.FC<{ creatorData: CreatorInfo; creatorAdd
         <div className="flex flex-row justify-center">
           <div className="md:text-sm text-[0.7rem] font-semibold flex flex-row items-center">
             <div className=" tracking-tighter px-3">Last:</div>
-            <div className=" tracking-tighter ">{getTimeAgo(last * 1000)}</div>
+            <div className=" tracking-tighter ">
+              {withdrawnEvents && withdrawnEvents.length > 0
+                ? getTimeAgo(withdrawnEvents[0]?.block.timestamp * 1000)
+                : "Never"}
+            </div>
           </div>
         </div>
       </div>
