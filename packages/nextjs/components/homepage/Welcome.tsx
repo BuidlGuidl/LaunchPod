525ff0dbc884e5abb1a8d762c6f5f62c73de9e33
Pipoca <58862302+cacosta88@users.noreply.github.com>
2023-10-06 15:09:36 -0300

    Update Welcome.tsx

--- packages/nextjs/components/homepage/Welcome.tsx
+++ packages/nextjs/components/homepage/Welcome.tsx
@@ -5,10 +5,12 @@ const Welcome = () => {
     <div className="md:text-sm text-[0.7rem] border rounded-xl p-4">
       <h1 className="font-bold font-typo-round md:text-2xl text-xl tracking-widest ">Welcome to LaunchPod!</h1>
       <p>
-        🚀 LaunchPod is on a mission to uplift the Ethereum ecosystem! 🌍 By offering monthly UBI to high-impact devs, we're spotlighting their invaluable contributions to our shared digital future. 🛠️
+        🚀 LaunchPod is on a mission to uplift the Ethereum ecosystem! 🌍 By offering monthly UBI to elite devs,
+        We&lsquo;re spotlighting their invaluable contributions to our shared digital future. 🛠️
       </p>
       <p>
-        🔗 Developers, this is your platform. Present your projects and access grants designed to amplify your impact within the Ethereum space. 💡
+        🔗 Developers, this is your platform. Present your projects and access grants designed to amplify your impact
+        within the Ethereum space. 💡
       </p>
       <p>This initiative is made possible by BuidlGuidl!</p>
     </div>
