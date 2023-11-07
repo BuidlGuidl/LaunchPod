dbfcdcd0c5eef2683f999982047701c8680658e0
avelous <avelousujiri@gmail.com>
2023-11-07 16:34:11 +0100

    contributions and token balance auto refresh And UX changes - modal exit on overlay click and input/label focus

--- packages/nextjs/components/homepage/WithdrawModal.tsx
+++ packages/nextjs/components/homepage/WithdrawModal.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import { EtherInput } from "../scaffold-eth";
 import { parseEther } from "viem";
 import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
@@ -13,6 +13,8 @@ export const WithdrawModal = ({
   const [reason, setReason] = useState("");
   const [amount, setAmount] = useState("0");
   const characterLimit = 400;
+
+  const labelRef = useRef<HTMLLabelElement | null>(null);
 
   const { writeAsync: withdraw } = useScaffoldContractWrite({
     contractName: "YourContract",
@@ -32,30 +34,68 @@ export const WithdrawModal = ({
     } else return;
   };
 
+  useEffect(() => {
+    const handleEscapeKey = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        closePopup();
+      }
+    };
+
+    const handleOverlayClick = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+
+      if (target.classList.contains("bg-gray-900")) {
+        closePopup();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("keydown", handleEscapeKey);
+      document.addEventListener("mousedown", handleOverlayClick);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKey);
+      document.removeEventListener("mousedown", handleOverlayClick);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (labelRef.current) {
+      labelRef.current.focus();
+    }
+  }, []);
+
   return (
-    <div className="border-2 rounded-xl overflow-hidden border-black w-fit text-xs bg-base-200 h-full">
+    <div className="">
       {isOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-20 md:text-sm text-[0.7rem]">
           <div className=" modal-box">
             <label onClick={closePopup} className="btn btn-sm btn-circle absolute right-2 top-2">
               ✕
             </label>
-            <label className="block mt-4 mb-2">Amount:</label>
-            <EtherInput
-              value={amount.toString()}
-              onChange={value => {
-                setAmount(value);
-              }}
-              placeholder="Enter withdraw amount"
-            />
-            <label className="block mt-2 mb-2">Reason:</label>
+            <label ref={labelRef}>
+              <h1 className="block mt-4 mb-2">Amount: </h1>
+              <EtherInput
+                value={amount.toString()}
+                onChange={value => {
+                  setAmount(value);
+                }}
+                placeholder="Enter withdraw amount"
+              />
+            </label>
 
-            <textarea
-              className="textarea textarea-bordered  focus:bg-transparent focus:text-gray-500 h-[2.2rem] min-h-[6.2rem] px-4  w-full font-medium placeholder:text-accent/50 text-gray-500 rounded-lg"
-              placeholder={`Enter Reason (limit: ${characterLimit} characters)`}
-              value={reason}
-              onChange={e => handleReasonChange(e.target.value)}
-            ></textarea>
+            <label>
+              <h1 className="block mt-2 mb-2">Reason:</h1>
+              <textarea
+                className="textarea textarea-bordered  focus:bg-transparent focus:text-gray-500 h-[2.2rem] min-h-[6.2rem] px-4  w-full font-medium placeholder:text-accent/50 text-gray-500 rounded-lg"
+                placeholder={`Enter Reason (limit: ${characterLimit} characters)`}
+                value={reason}
+                onChange={e => handleReasonChange(e.target.value)}
+              ></textarea>
+            </label>
+
             <p className="font-sans italic font-normal -mt-1">Remaining characters: {characterLimit - reason.length}</p>
             <button className="btn btn-primary rounded-lg w-full mt-2 ml-auto" onClick={() => withdraw()}>
               Withdraw
