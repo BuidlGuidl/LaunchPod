17170be2e9d626a5c1ee72b6d72b1b62443309d5
Zak G <zakgriffith@gmail.com>
2023-12-13 06:56:56 -0700

    Update MetaHeader.tsx

--- packages/nextjs/components/MetaHeader.tsx
+++ packages/nextjs/components/MetaHeader.tsx
@@ -14,7 +14,7 @@ type MetaHeaderProps = {
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/` : "/";
 
 export const MetaHeader = ({
-  title = "LaunchPod™",
+  title = "LaunchPod",
   description = "LaunchPod: Created with 🏗 scaffold-eth-2",
   image = "thumbnail.jpg",
   twitterCard = "summary_large_image",
