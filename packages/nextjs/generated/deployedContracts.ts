f74d8d77a88692c200ebd6490eca10d181c629e1
pipoquinha <caio.costa@me.com>
2023-12-20 17:50:48 -0300

    update primary adminv4

--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -5,7 +5,7 @@ const contracts = {
       name: "localhost",
       contracts: {
         ERC20Mock1: {
-          address: "0x9A676e781A523b5d0C0e43731313A708CB607508",
+          address: "0x322813Fd9A801c5507c9de605d63CEA4f2CE6c44",
           abi: [
             {
               inputs: [],
@@ -347,7 +347,7 @@ const contracts = {
           ],
         },
         ERC20Mock2: {
-          address: "0x0B306BF915C4d645ff596e518fAf3F9669b97016",
+          address: "0xa85233C63b9Ee964Add6F2cffe00Fd84eb32338f",
           abi: [
             {
               inputs: [],
@@ -689,7 +689,7 @@ const contracts = {
           ],
         },
         YourContract: {
-          address: "0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1",
+          address: "0x4A679253410272dd5232B3Ff7cF5dbB88f295319",
           abi: [
             {
               inputs: [
